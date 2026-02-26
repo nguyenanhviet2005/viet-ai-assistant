ee38c3e47e19428dd2421114b047a126b25a5876
nguyenanhviet2005 <nguyenanhviet2005@gmail.com>
2026-02-26 23:05:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
 import streamlit as st
 from openai import OpenAI
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 st.title("Viet AI Assistant")
 
